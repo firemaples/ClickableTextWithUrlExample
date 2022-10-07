d8866ff5542e3e02ed5d0a03e9da31bc55af6a10
Chantell <chantell@98point6.com>
2022-10-06 17:17:29 -0700

    Initial commit for repo of bug

---
+++ app/src/main/java/com/example/accessibilitybug/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.accessibilitybug
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.UrlAnnotation
+import androidx.compose.ui.text.buildAnnotatedString
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalTextApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LinkClickableText(annotatedString = buildAnnotatedString {
+                append("Not a link, but ")
+                pushUrlAnnotation(UrlAnnotation(url = "https://www.google.com"))
+                append("this is a link.")
+                pop()
+                append(" And here's another string without a link.")
+            })
+        }
+    }
+}
+
+@OptIn(ExperimentalTextApi::class)
+@Composable
+private fun LinkClickableText(
+    annotatedString: AnnotatedString
+) {
+    val uriHandler = LocalUriHandler.current
+    ClickableText(
+        modifier = Modifier,
+        text = annotatedString,
+        onClick = { offset ->
+            annotatedString.getUrlAnnotations(
+                start = offset,
+                end = offset
+            ).firstOrNull()?.let { annotation ->
+                uriHandler.openUri(annotation.item.url)
+            }
+        }
+    )
+}
